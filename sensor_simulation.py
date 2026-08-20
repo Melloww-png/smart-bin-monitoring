5ae16f05ca36a79c5b53dc6c541d33cae244de9c
Daniel CH <danielclementrich09@gmail.com>
2026-08-21 00:21:13 +0800

    Add sensor simulation

---
+++ sensor_simulation.py
@@ -0,0 +1,12 @@
+#Sensor simulatiion
+
+import random
+
+bin_height = 100
+
+distance = random.randint(2, 100)  # Simulated distance reading from the ultrasonic sensor
+
+print("Ultrasonic Sensor Simulation")
+print("----------------------------")
+print(f"Bin Height: {bin_height} cm")
+print(f"Sensor Distance: {distance} cm")
