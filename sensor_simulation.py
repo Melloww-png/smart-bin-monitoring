6a0a9e00214b9a7dcc3182150390e964e73b4776
Daniel CH <danielclementrich09@gmail.com>
2026-08-25 16:29:36 +0800

    added alert system and multiple bin stimulation

--- sensor_simulation.py
+++ sensor_simulation.py
@@ -4,31 +4,51 @@
 import random
 
 bin_height = 100
+number_of_bins = 3
 
-distance = random.randint(2, 100)  # Simulated distance reading from the ultrasonic sensor
+print("Smart Public Bin Monitoring System")
+print("==================================")
 
-print("Ultrasonic Sensor Simulation")
-print("----------------------------")
-print(f"Bin Height: {bin_height} cm")
-print(f"Sensor Distance: {distance} cm")
-fill_level = ((bin_height - distance) / bin_height) * 100
+for bin_number in range(1, number_of_bins + 1):
 
-if fill_level >= 100:
-    status = "Full - Immediate Collection Required"
-    bin_access = "CLOSED - Not Accepting More Trash"
-elif fill_level < 20:
-    status = "Less than half"
-    bin_access = "OPEN"
-elif fill_level < 50:
-    status = "Half full"
-    bin_access = "OPEN"
-elif fill_level < 80:
-    status = "Getting Full"
-    bin_access = "OPEN"
-elif fill_level < 100:
-    status = "Nearly Full"
-    bin_access = "OPEN"
+    # Simulated the ultrasonic sensor reading
+    distance = random.randint(0, 100)
 
-print(f"Fill Level: {fill_level:.1f}%")
-print(f"Bin Status: {status}")
-print(f"Bin Access: {bin_access}")
+    # Calculate fill level
+    fill_level = ((bin_height - distance) / bin_height) * 100
+
+    # Determine bin status, access and alert
+    if fill_level >= 100:
+        status = "Full"
+        bin_access = "CLOSED - Not Accepting More Trash"
+        alert = "ALERT: Immediate Collection Required!"
+
+    elif fill_level >= 80:
+        status = "Nearly Full"
+        bin_access = "OPEN"
+        alert = "ALERT: Collection Required Soon!"
+
+    elif fill_level >= 50:
+        status = "Half Full"
+        bin_access = "OPEN"
+        alert = "WARNING: Bin is Half Full"
+
+    elif fill_level >= 20:
+        status = "Less Than Half Full"
+        bin_access = "OPEN"
+        alert = "No Alert - Bin Has Enough Space"
+
+    else:
+        status = "Almost Empty"
+        bin_access = "OPEN"
+        alert = "No Alert - Bin is Close to Empty"
+
+    # Display results
+    print()
+    print(f"Bin {bin_number}")
+    print("----------------------------")
+    print(f"Sensor Distance: {distance} cm")
+    print(f"Fill Level: {fill_level:.1f}%")
+    print(f"Bin Status: {status}")
+    print(f"Bin Access: {bin_access}")
+    print(f"System Alert: {alert}")
